bc0f8a990ba2fc53419f41d35697529e9e076d95
Sami Korhonen <sami.korhonen@helsinki.fi>
2014-03-17 13:12:12 +0200

    uutta

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Tietokantasovellus</title>
+    <meta charset="utf-8" />
+    <style>
+      h1 { font-size: 1.4em; }
+      h2 { font-size: 1.1em; }
+    </style>
+  </head>
+  <body>
+    <h1>Tietokantasovelluksen esittelysivu</h1>
+    <a href="esittelysivu.html">Esittelysivu</a>
+  </body>
+</html>
